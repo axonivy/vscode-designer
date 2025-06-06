43b1e8b03305ff72657308781895bdccece513b1
Christian Strebel <christian.strebel@axonivy.com>
2025-06-06 16:28:38 +0200

    Use engine 13.1 on release 13.1 branch

--- .ivy/raise-deps.sh
+++ .ivy/raise-deps.sh
@@ -5,7 +5,7 @@ shopt -s globstar
 # likely not working on mac
 
 # This should only be done if branch is not master...
-# sed -i -E "s|core_product-engine/job/master/|core_product-engine/job/release%2F${1/.[0-9]-SNAPSHOT/}/|" build/**/Jenkinsfile
+# sed -i -E "s|core_product-engine/job/release%252F13.1/|core_product-engine/job/release%2F${1/.[0-9]-SNAPSHOT/}/|" build/**/Jenkinsfile
 sed -i -E "s/(\"@axonivy[^\"]*\"): \"[^\"]*\"/\1: \"~${1/SNAPSHOT/next}\"/" package.json
 sed -i -E "s/(\"@axonivy[^\"]*\"): \"[^\"]*\"/\1: \"~${1/SNAPSHOT/next}\"/" extension/package.json
 sed -i -E "s/(\"@axonivy[^\"]*\"): \"[^\"]*\"/\1: \"~${1/SNAPSHOT/next}\"/" webviews/*/package.json
