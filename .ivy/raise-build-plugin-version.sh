e904f3f4cfe6c3891586864dd79fd971eec667bc
Louis <louis.mueller@axonivy.com>
2025-06-10 12:03:26 +0200

    remove raise build plugin (#599)

--- .ivy/raise-build-plugin-version.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mvn --batch-mode versions:set-property versions:commit -f playwright/tests/workspaces/prebuiltProject/pom.xml -Dproperty=project.build.plugin.version -DnewVersion=${2} -DallowSnapshots=true
