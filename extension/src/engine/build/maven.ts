804fd4cd5a6abf88b2a5a50b7ab00653d64b1b86
ivy-team <info@ivyteam.ch>
2025-06-27 12:32:38 +0000

    Raise version to 13.1.2-SNAPSHOT

--- extension/src/engine/build/maven.ts
+++ extension/src/engine/build/maven.ts
@@ -5,7 +5,7 @@ import { XMLParser } from 'fast-xml-parser';
 import path from 'path';
 import { config } from '../../base/configurations';
 
-const IVY_ENGINE_VERSION = '13.1.1';
+const IVY_ENGINE_VERSION = '13.1.2';
 
 export class MavenBuilder {
   private readonly outputChannel: vscode.OutputChannel;
