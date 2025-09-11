9c6bf09c02ed562fdd424374242b11491352695f
Louis <louis.mueller@axonivy.com>
2025-09-11 14:53:31 +0200

    langauge client

--- extension/src/index.ts
+++ extension/src/index.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import * as vscode from 'vscode';
+import { LanguageClient, LanguageClientOptions, StreamInfo } from 'vscode-languageclient/node';
 import { Messenger, MessengerDiagnostic } from 'vscode-messenger';
 import { registerCommand } from './base/commands';
 import { config } from './base/configurations';
@@ -10,7 +11,11 @@ import { IvyEngineManager } from './engine/engine-manager';
 import { showRuntimeLog } from './engine/ws-client';
 import { IvyProjectExplorer } from './project-explorer/ivy-project-explorer';
 
+import { IWebSocket, WebSocketMessageReader, WebSocketMessageWriter } from 'vscode-ws-jsonrpc';
+
 let ivyEngineManager: IvyEngineManager;
+
+let languageClient: LanguageClient;
 
 export const messenger = new Messenger({ ignoreHiddenViews: false });
 
@@ -18,13 +23,15 @@ export const downloadDevEngine = () =>
   vscode.env.openExternal(vscode.Uri.parse('https://dev.axonivy.com/permalink/dev/axonivy-engine-slim.zip'));
 
 export async function activate(context: vscode.ExtensionContext): Promise<MessengerDiagnostic> {
-  const vscodeXml = await vscode.extensions.getExtension('redhat.vscode-xml')?.activate();
-  if (vscodeXml) {
-    vscodeXml.addXMLCatalogs([]);
-  }
+  // const vscodeXml = await vscode.extensions.getExtension('redhat.vscode-xml')?.activate();
+  // if (vscodeXml) {
+  //   const catalog = vscode.Uri.joinPath(context.extensionUri, 'assets', 'xml', 'catalog', 'catalog.xml').fsPath;
+  //   vscodeXml.addXMLCatalogs([catalog]);
+  // }
+
+  await startLanguageClient();
 
   ivyEngineManager = IvyEngineManager.init(context);
-
   registerCommand('engine.deployProjects', context, () => ivyEngineManager.deployProjects());
   registerCommand('engine.buildProjects', context, () => ivyEngineManager.buildProjects());
   registerCommand('engine.buildAndDeployProjects', context, () => ivyEngineManager.buildAndDeployProjects());
@@ -41,6 +48,56 @@ export async function activate(context: vscode.ExtensionContext): Promise<Messen
   return messenger.diagnosticApi();
 }
 
+const startLanguageClient = async () => {
+  const url = 'ws://localhost:8080/lsp/bla';
+
+  const serverOptions = (): Promise<StreamInfo> => {
+    const webSocket = new WebSocket(url);
+    return new Promise((resolve, reject) => {
+      webSocket.onopen = () => {
+        resolve({
+          reader: new WebSocketMessageReader(toSocket(webSocket)),
+          writer: new WebSocketMessageWriter(toSocket(webSocket))
+        } as unknown as StreamInfo);
+      };
+      webSocket.onerror = reject;
+    });
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: ['xhtml'],
+    synchronize: {
+      fileEvents: vscode.workspace.createFileSystemWatcher('**/*.xhtml')
+    }
+  };
+
+  languageClient = new LanguageClient('xhtml server', serverOptions, clientOptions);
+
+  languageClient.start();
+};
+
 export async function deactivate() {
   await ivyEngineManager.stop();
+  await languageClient?.stop();
 }
+
+const toSocket = (webSocket: WebSocket): IWebSocket => {
+  return {
+    send: content => webSocket.send(content),
+    onMessage: cb => {
+      webSocket.onmessage = event => cb(event.data);
+    },
+    onError: cb => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      webSocket.onerror = (event: any) => {
+        if (Object.hasOwn(event, 'message')) {
+          cb(event.message);
+        }
+      };
+    },
+    onClose: cb => {
+      webSocket.onclose = event => cb(event.code, event.reason);
+    },
+    dispose: () => webSocket.close()
+  };
+};
