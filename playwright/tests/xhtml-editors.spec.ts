06a66bba7c360eddd6c916b34e3d333e05f53fcb
ivy-jh <jens.hartmann@axonivy.com>
2025-10-30 15:09:50 +0100

    add tests

--- playwright/tests/xhtml-editors.spec.ts
+++ playwright/tests/xhtml-editors.spec.ts
@@ -1,22 +1,43 @@
 import { expect, test } from './fixtures/baseTest';
 import { XhtmlEditor } from './page-objects/xhtml-editor';
 
-
-  test('XHTML Editor completion', async ({ page }) => {
+  test('primeface_tag_completion', async ({ page }) => {
     const editor = new XhtmlEditor(page);
     const completionDialogSelector = '.suggest-widget';
-    const firstCompletionItemSelector = '.monaco-list-row .label-name';
     await editor.openEditorFile();
     await editor.hasNoStatusMessage();
-    await page.getByText("<p:messages").click();
-    await page.keyboard.press("Control+Space");
+    await page.getByText('<p:messages').click();
+    await page.keyboard.press('Control+Space');
     const completionDialog = page.locator(completionDialogSelector);
-    await expect(completionDialog).toBeVisible({ timeout: 5000 });
-    const firstCompletionItem = page.locator(firstCompletionItemSelector).first();
-    await expect(firstCompletionItem).toBeVisible();
-    const firstItemText = await firstCompletionItem.innerText();
-    console.log(firstItemText);
-    const expectedText = 'p:media';
-    console.log(expectedText);
-    await expect(firstCompletionItem).toBeVisible();
+    await expect(completionDialog).toBeVisible();
+    await expect(completionDialog.getByText('p:media')).toBeVisible();
+  });
+
+  test('primeface_attr_compition', async ({ page }) => {
+    const editor = new XhtmlEditor(page);
+    const completionDialogSelector = '.suggest-widget';
+    await editor.openEditorFile();
+    await editor.hasNoStatusMessage();
+    await page.getByText('<p:messages />').click();
+    await page.keyboard.press('ArrowRight+ArrowRight+ArrowRight+ArrowRight');
+    await page.keyboard.press('ArrowRight+ArrowRight+ArrowRight+ArrowRight');
+    await page.keyboard.press('Control+Space');
+    const completionDialog = page.locator(completionDialogSelector);
+    await expect(completionDialog).toBeVisible();
+    await expect(completionDialog.getByText('rendered')).toBeVisible();
+    await page.keyboard.press('ArrowDown'); //go to 2nd attribute closable
+    await expect(page.getByText('Adds a close icon to hide the messages')).toBeVisible();
+  });
+
+  test('htmlBasic_tag_completion', async ({ page }) => {
+    const editor = new XhtmlEditor(page);
+    const completionDialogSelector = '.suggest-widget';
+    await editor.openEditorFile();
+    await editor.hasNoStatusMessage();
+    await page.getByText('<h:form').click();
+    const completionDialog = page.locator(completionDialogSelector);
+    await page.keyboard.press('ArrowLeft+ArrowLeft+ArrowLeft+ArrowLeft');
+    await page.keyboard.press('Control+Space');
+    await expect(completionDialog).toBeVisible();
+    await expect(completionDialog.getByText('h:body')).toBeVisible();
   });
