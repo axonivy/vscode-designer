25829cd201d23a9500e9af6c5832ab0a80814794
Lukas Lieb <lukas.lieb@axonivy.com>
2025-11-20 11:31:46 +0100

    Fix debug

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -13,7 +13,24 @@ const config = {
     library: {
       type: 'commonjs'
     },
-    devtoolModuleFilenameTemplate: '../[resource-path]'
+    devtoolModuleFilenameTemplate: info => {
+      const absPath = info.absoluteResourcePath;
+
+      // For our TypeScript source files, return absolute path from workspace root
+      if (absPath.includes('/extension/src/')) {
+        const srcIndex = absPath.indexOf('/extension/src/');
+        const workspaceRelativePath = 'extension/' + absPath.substring(srcIndex + '/extension/'.length);
+        return workspaceRelativePath;
+      } else if (absPath.includes('\\extension\\src\\')) {
+        const srcIndex = absPath.indexOf('\\extension\\src\\');
+        const workspaceRelativePath = 'extension/' + absPath.substring(srcIndex + '\\extension\\'.length).replace(/\\/g, '/');
+        return workspaceRelativePath;
+      }
+
+      // For node_modules and other external files, use standard relative path
+      const rel = path.relative(__dirname, absPath);
+      return path.posix.join('..', rel.replace(/\\/g, '/'));
+    }
   },
   devtool: 'source-map',
   externals: {
@@ -22,7 +39,8 @@ const config = {
     bufferutil: 'bufferutil'
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
+    symlinks: true
   },
   module: {
     rules: [
@@ -34,7 +52,8 @@ const config = {
             loader: 'ts-loader',
             options: {
               compilerOptions: {
-                module: 'es6'
+                module: 'es6',
+                sourceMap: true
               }
             }
           }
