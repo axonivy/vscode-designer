0a7045b2cf1878450407422cd3c306c3d7fbfcc7
Lukas Lieb <lukas.lieb@axonivy.com>
2025-12-12 08:21:27 +0100

    Show process editor grid per default
    - Improve dx on vite configs
    - Fix gird color
    - Fix process initial loading

--- webviews/form-editor/vite.config.ts
+++ webviews/form-editor/vite.config.ts
@@ -1,19 +1,16 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(() => {
-  const config = {
-    plugins: [tsconfigPaths()],
-    build: {
-      manifest: 'build.manifest.json',
-      outDir: '../../extension/dist/webviews/form-editor',
-      chunkSizeWarningLimit: 5000
-    },
-    server: {
-      port: 3000,
-      open: false
-    },
-    base: './'
-  };
-  return config;
-});
+export default defineConfig(() => ({
+  plugins: [tsconfigPaths()],
+  build: {
+    manifest: 'build.manifest.json',
+    outDir: '../../extension/dist/webviews/form-editor',
+    chunkSizeWarningLimit: 5000
+  },
+  server: {
+    port: 3000,
+    open: false
+  },
+  base: './'
+}));
--- webviews/process-editor/vite.config.ts
+++ webviews/process-editor/vite.config.ts
@@ -2,34 +2,31 @@ import monacoConfigPlugin from '@axonivy/monaco-vite-plugin';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(() => {
-  const config = {
-    plugins: [
-      tsconfigPaths(),
-      monacoConfigPlugin({
-        ignorePackages: [
-          'vscode-messenger-webview',
-          'vscode-messenger-common',
-          '@eclipse-glsp/vscode-integration',
-          '@eclipse-glsp/vscode-integration-webview',
-          '@axonivy/vscode-webview-common'
-        ]
-      })
-    ],
-    build: {
-      manifest: 'build.manifest.json',
-      outDir: '../../extension/dist/webviews/process-editor',
-      chunkSizeWarningLimit: 5000
-    },
-    server: {
-      port: 3000,
-      open: false,
-      sourcemapIgnoreList(sourcePath: string, sourcemapPath: string) {
-        return sourcePath.includes('node_modules') && !sourcePath.includes('@eclipse-glsp') && !sourcePath.includes('@axonivy');
-      }
-    },
-    resolve: { alias: { path: 'path-browserify' } },
-    base: './'
-  };
-  return config;
-});
+export default defineConfig(() => ({
+  plugins: [
+    tsconfigPaths(),
+    monacoConfigPlugin({
+      ignorePackages: [
+        'vscode-messenger-webview',
+        'vscode-messenger-common',
+        '@eclipse-glsp/vscode-integration',
+        '@eclipse-glsp/vscode-integration-webview',
+        '@axonivy/vscode-webview-common'
+      ]
+    })
+  ],
+  build: {
+    manifest: 'build.manifest.json',
+    outDir: '../../extension/dist/webviews/process-editor',
+    chunkSizeWarningLimit: 5000
+  },
+  server: {
+    port: 3000,
+    open: false,
+    sourcemapIgnoreList(sourcePath: string, sourcemapPath: string) {
+      return sourcePath.includes('node_modules') && !sourcePath.includes('@eclipse-glsp') && !sourcePath.includes('@axonivy');
+    }
+  },
+  resolve: { alias: { path: 'path-browserify' } },
+  base: './'
+}));
