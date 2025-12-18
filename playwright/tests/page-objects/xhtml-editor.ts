58610ff540c9e24a15c3ce53c6135045c6497c5c
Louis <louis.mueller@axonivy.com>
2025-12-18 16:14:39 +0100

    try to fix flaky xhtml 13.2

--- playwright/tests/page-objects/xhtml-editor.ts
+++ playwright/tests/page-objects/xhtml-editor.ts
@@ -1,11 +1,18 @@
-import { type Locator, type Page } from '@playwright/test';
+import { expect, type Locator, type Page } from '@playwright/test';
 import { Editor } from './editor';
 
 export class XhtmlEditor extends Editor {
   readonly complitions: Locator;
-  
+
   constructor(page: Page, editorFile = 'testXhtml.xhtml') {
     super(editorFile, page);
     this.complitions = page.locator('.suggest-widget');
+  }
+
+  async expectCompletionAtLineColumn(completion: string, line: number, column: number) {
+    await this.executeCommand('Go to Line/Column...', `:${line}:${column}`);
+    await this.page.waitForTimeout(300);
+    await this.executeCommand('Trigger Suggest');
+    await expect(this.complitions.getByText(completion)).toBeVisible();
   }
 }
