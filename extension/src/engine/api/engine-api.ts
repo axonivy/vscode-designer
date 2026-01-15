143687a59cacfc9d350b3704ded1e27b26775a85
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2026-01-15 08:44:05 +0100

    XIVY-18222 flag workspace as 'vsc' for std Maven
    - resolve market products with existing Maven environment

--- extension/src/engine/api/engine-api.ts
+++ extension/src/engine/api/engine-api.ts
@@ -63,7 +63,7 @@ export class IvyEngineApi {
     if (workspaces.length > 1) {
       throw new Error('Too many workspaces available, only one workspace is supported');
     }
-    const workspaceInit = { name: workspace.name, path: workspace.uri.fsPath };
+    const workspaceInit = { name: workspace.name, path: workspace.uri.fsPath, neo: false };
     return await vscode.window.withProgress(progressOptions('Create workspace'), async () => {
       return (await createWorkspace(workspaceInit, { baseURL, ...options })).data;
     });
