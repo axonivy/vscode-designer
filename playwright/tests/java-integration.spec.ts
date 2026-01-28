e7dbdd659d6142e3c9b73cdc9046f4493e87d0a2
Louis <louis.mueller@axonivy.com>
2026-01-28 10:57:51 +0100

    w

--- playwright/tests/java-integration.spec.ts
+++ playwright/tests/java-integration.spec.ts
@@ -24,6 +24,7 @@ test('Compile java and invalidate class loader', async ({ page }) => {
   await javaEditor.saveAllFiles();
   await javaEditor.hasStatusMessage('Finished: Invalidate class loader');
 
+  await processEditor.openEditorFile();
   const start = processEditor.locatorForPID('19BE060A6564078E-f0');
   const end = processEditor.locatorForPID('19BE060A6564078E-f1');
   await processEditor.startProcessAndAssertExecuted(start, end);
