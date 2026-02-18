99d77bcdc5fc900ddb3a5e6a0ad73bf123c8530c
Louis <louis.mueller@axonivy.com>
2026-02-18 16:23:34 +0100

    XIVY-18232 validate and sync java version (#973)

---
+++ extension/src/base/java-version-validation.test.ts
@@ -0,0 +1,72 @@
+import path from 'path';
+import { expect, test, vi } from 'vitest';
+import { validateAndSyncJavaVersion } from './java-version-validation';
+
+vi.mock('vscode', () => ({
+  window: {
+    createOutputChannel: () => ({
+      error: vi.fn(),
+      warn: vi.fn(),
+      info: vi.fn()
+    }),
+    showErrorMessage: vi.fn(),
+    showWarningMessage: vi.fn(),
+    showInformationMessage: vi.fn()
+  },
+  workspace: {
+    getConfiguration: () => ({
+      get: vi.fn(),
+      update: vi.fn()
+    })
+  },
+  ConfigurationTarget: {
+    Global: 1
+  },
+  commands: {
+    executeCommand: vi.fn()
+  }
+}));
+
+const testDir = (javaHome: string) => path.join(__dirname, 'java-version-validation-test', javaHome);
+
+test('Undefined JAVA_HOME and IVY_JAVA_HOME', async () => {
+  vi.stubEnv('JAVA_HOME', undefined);
+  vi.stubEnv('IVY_JAVA_HOME', undefined);
+  await expect(validateAndSyncJavaVersion()).rejects.toThrow(
+    `No valid Java found under JAVA_HOME=undefined or java.jdt.ls.java.home=undefined.
+    Either set env variable JAVA_HOME to valid Java 25 installation path,
+    or configure VS Code setting 'java.jdt.ls.java.home'.`
+  );
+});
+
+test('Valid JAVA_HOME', async () => {
+  vi.stubEnv('JAVA_HOME', testDir('java25'));
+  vi.stubEnv('IVY_JAVA_HOME', undefined);
+  await expect(validateAndSyncJavaVersion()).resolves.toBeUndefined();
+});
+
+test('Valid IVY_JAVA_HOME', async () => {
+  vi.stubEnv('JAVA_HOME', testDir('java21'));
+  vi.stubEnv('IVY_JAVA_HOME', testDir('java25'));
+  await expect(validateAndSyncJavaVersion()).resolves.toBeUndefined();
+});
+
+test('Invalid IVY_JAVA_HOME', async () => {
+  const java21Dir = testDir('java21');
+  vi.stubEnv('JAVA_HOME', undefined);
+  vi.stubEnv('IVY_JAVA_HOME', java21Dir);
+  await expect(validateAndSyncJavaVersion()).rejects
+    .toThrow(`No valid Java found under JAVA_HOME=${java21Dir} or java.jdt.ls.java.home=undefined.
+    Either set env variable JAVA_HOME to valid Java 25 installation path,
+    or configure VS Code setting 'java.jdt.ls.java.home'.`);
+});
+
+test('Invalid IVY_JAVA_HOME', async () => {
+  const java21Dir = testDir('java21');
+  vi.stubEnv('JAVA_HOME', java21Dir);
+  vi.stubEnv('IVY_JAVA_HOME', undefined);
+  await expect(validateAndSyncJavaVersion()).rejects
+    .toThrow(`No valid Java found under JAVA_HOME=${java21Dir} or java.jdt.ls.java.home=undefined.
+    Either set env variable JAVA_HOME to valid Java 25 installation path,
+    or configure VS Code setting 'java.jdt.ls.java.home'.`);
+});
---
+++ extension/src/base/java-version-validation.ts
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+import * as vscode from 'vscode';
+import { logErrorMessage, logInformationMessage } from './logging-util';
+
+const EXPECTED_JAVA_VERSION = '25';
+
+export const validateAndSyncJavaVersion = async () => {
+  const javaHome = process.env.IVY_JAVA_HOME ?? process.env.JAVA_HOME;
+  const jdtJavaHome = vscode.workspace.getConfiguration().get<string>('java.jdt.ls.java.home');
+  const isValidJavaHome = isValidJavaVersion(javaHome);
+  const isValidJdtJavaHome = isValidJavaVersion(jdtJavaHome);
+  if (!isValidJavaHome && !isValidJdtJavaHome) {
+    const message = `No valid Java found under JAVA_HOME=${javaHome} or java.jdt.ls.java.home=${jdtJavaHome}.
+    Either set env variable JAVA_HOME to valid Java ${EXPECTED_JAVA_VERSION} installation path,
+    or configure VS Code setting 'java.jdt.ls.java.home'.`;
+    logErrorMessage(message);
+    throw new Error(message);
+  }
+  if (!isValidJavaHome) {
+    process.env.IVY_JAVA_HOME = jdtJavaHome; // will be used for engine start
+    return;
+  }
+  if (!isValidJdtJavaHome) {
+    logInformationMessage("Updating 'java.jdt.ls.java.home' to match JAVA_HOME and restarting extension host...");
+    await vscode.workspace.getConfiguration().update('java.jdt.ls.java.home', javaHome, vscode.ConfigurationTarget.Global);
+    await vscode.commands.executeCommand('workbench.action.reloadWindow');
+  }
+};
+
+const isValidJavaVersion = (javaHome?: string) => {
+  if (!javaHome) {
+    return false;
+  }
+  const releasePath = path.join(javaHome, 'release');
+  try {
+    const releaseContent = fs.readFileSync(releasePath, 'utf8');
+    const javaVersion = releaseContent.split('\n').find(line => line.startsWith('JAVA_VERSION='));
+    return javaVersion?.startsWith(`JAVA_VERSION="${EXPECTED_JAVA_VERSION}.`);
+  } catch {
+    return false;
+  }
+};
