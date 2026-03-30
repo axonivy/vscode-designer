6d881db1f11862df7a737b86714e0ff6054e6192
Louis <louis.mueller@axonivy.com>
2026-03-30 13:23:30 +0200

    w

--- playwright/tests/integration/runtime-log.spec.ts
+++ playwright/tests/integration/runtime-log.spec.ts
@@ -19,8 +19,8 @@ test.describe.only('Runtime Log', () => {
     await processEditor.executeCommand('Axon Ivy: Open Axon Ivy Runtime Log');
 
     const runtimeLogOutput = page.getByRole('document', { name: 'Runtime Log - Output' }).getByRole('code');
-    await runtimeLogOutput.focus();
     await runtimeLogOutput.press('PageUp');
+    await runtimeLogOutput.focus();
     await runtimeLogOutput.press('Control+Home');
 
     await expect(runtimeLogOutput).toContainText('[info]');
