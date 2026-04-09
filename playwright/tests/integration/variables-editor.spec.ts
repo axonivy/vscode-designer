f7d3c2af0323e2f7ebc085056d1799a71e705278
Louis <louis.mueller@axonivy.com>
2026-04-09 16:55:31 +0200

    w

--- playwright/tests/integration/variables-editor.spec.ts
+++ playwright/tests/integration/variables-editor.spec.ts
@@ -22,8 +22,7 @@ test('Read, write', async ({ page }) => {
   await expect(editor.editorContent()).toContainText(`originalKey: ${newValue}`);
 });
 
-// eslint-disable-next-line playwright/no-focused-test
-test.only('Not possible to open multiple dialogs using shortcut', async ({ page }) => {
+test('Not possible to open multiple dialogs using shortcut', async ({ page }) => {
   const editor = new VariablesEditor(page);
   await editor.hasDeployProjectStatusMessage();
   await editor.openEditorFile();
