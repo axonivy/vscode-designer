f0be7951f26457aec4b04d90fa11cf21674930da
ivy-dar <dominik.arnold@axonivy.com>
2026-06-12 09:28:37 +0200

    activate all tests

--- playwright/tests/integration/create-project.spec.ts
+++ playwright/tests/integration/create-project.spec.ts
@@ -5,8 +5,7 @@ import { ProblemsView } from '../page-objects/problems-view';
 import { ProcessEditor } from '../page-objects/process-editor';
 import { empty } from '../workspaces/workspace';
 
-// eslint-disable-next-line playwright/no-focused-test
-test.describe.only('Create Project', () => {
+test.describe('Create Project', () => {
   test.use({ workspace: empty });
 
   test('Add Project and execute init Process', async ({ page }) => {
