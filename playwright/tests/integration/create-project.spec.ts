01f28c320fae24c01e6aa6c3eae68278416e8db4
Louis <louis.mueller@axonivy.com>
2026-06-15 15:25:13 +0200

    q

--- playwright/tests/integration/create-project.spec.ts
+++ playwright/tests/integration/create-project.spec.ts
@@ -23,7 +23,7 @@ test.describe('Create Project', () => {
     await processEditor.hasBreadCrumbs('parent', projectName, 'processes', projectName, 'BusinessProcess.p.json');
     const start = processEditor.locatorForElementType('g.start\\:requestStart');
     const end = processEditor.locatorForElementType('g.end\\:taskEnd');
-    await page.waitForTimeout(5_000);
+    await page.waitForTimeout(10_000);
     await processEditor.startProcessAndAssertExecuted(start, end);
   });
 });
