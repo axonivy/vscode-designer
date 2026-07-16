31f0c870ede793bfc60fa0e830121569b0b20ad0
ivy-dar <dominik.arnold@axonivy.com>
2026-07-16 11:19:43 +0200

    Remove download-iar unit test

--- playwright/vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    include: ['**/*.test.ts'],
-    reporters: process.env.CI ? ['default', 'junit'] : ['default'],
-    outputFile: 'report.xml'
-  }
-});
