3795a824131cb47eac8181e2a3a9ab0a1f68920e
ivy-dar <dominik.arnold@axonivy.com>
2026-07-16 14:22:32 +0200

    add print stmt

--- playwright/tests/test.setup.ts
+++ playwright/tests/test.setup.ts
@@ -5,6 +5,9 @@ import { runDownloadAndUnzipVSCode } from './utils/download-vscode';
 setup('Setup', async ({}) => {
   console.log('Starting setup...');
 
+  console.log('All process.env:', process.env);
+  console.log('All process.argv:', process.argv);
+
   const skipIarDownload = process.env.SKIP_IAR_DOWNLOAD ? true : false;
 
   await runDownloadAndUnzipVSCode();
