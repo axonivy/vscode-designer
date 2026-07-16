4a48e6fc2dbc262c0fb13bb3667e33f242eac947
ivy-dar <dominik.arnold@axonivy.com>
2026-07-16 15:39:40 +0200

    Fix test

--- playwright/tests/integration/explorer/import-ivy-project.spec.ts
+++ playwright/tests/integration/explorer/import-ivy-project.spec.ts
@@ -7,7 +7,7 @@ test.use({ workspace: empty });
 test('Import up-to-date Ivy Project', async ({ wsPage }) => {
   const explorer = new FileExplorer(wsPage);
   await explorer.hasNodeExact(emptyDownloadIarFilenameUptodate);
-  await wsPage.executeCommand('Axon Ivy: Import Ivy Project');
+  await wsPage.executeCommand('Import Axon Ivy Project Archive (.iar or .zip)');
   await wsPage.selectItemFromQuickPick(emptyDownloadIarFilenameUptodate);
   await wsPage.executeCommand('Refresh Explorer');
   await explorer.hasNodeExact(emptyDownloadIarFilenameUptodate.replace('.iar', ''));
