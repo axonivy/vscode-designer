eb234b7f4eb663a871257fda1f716e30259574e2
ivy-dar <dominik.arnold@axonivy.com>
2026-07-16 13:55:14 +0200

    Fix setup test code

--- playwright/tests/test.setup.ts
+++ playwright/tests/test.setup.ts
@@ -1,11 +1,10 @@
+import { test as setup } from '@playwright/test';
 import { runDownloadIar } from './utils/download-iar';
 import { runDownloadAndUnzipVSCode } from './utils/download-vscode';
 
-const runSetup = async () => {
+setup('Setup', async ({}) => {
   console.log('Starting setup...');
   await runDownloadAndUnzipVSCode();
   await runDownloadIar();
   console.log('Setup completed.');
-};
-
-runSetup();
+});
