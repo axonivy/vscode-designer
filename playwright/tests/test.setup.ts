b1638b3f88c40374e3e025c92e0b34c93bfb2be8
ivy-dar <dominik.arnold@axonivy.com>
2026-07-16 16:23:04 +0200

    wip: Supply iar arguments via Jenkins params into env var

--- playwright/tests/test.setup.ts
+++ playwright/tests/test.setup.ts
@@ -3,6 +3,9 @@ import { runDownloadIar } from './utils/download-iar';
 import { runDownloadAndUnzipVSCode } from './utils/download-vscode';
 
 setup('Setup', async ({}) => {
+  console.log('test.setup.ts: process.argv: ' + process.argv.join('\n'));
+  console.log('test.setup.ts: process.env: ' + JSON.stringify(process.env, null, 2));
+
   const skipIarDownload = process.env.SKIP_IAR_DOWNLOAD ? true : false;
   await runDownloadAndUnzipVSCode();
   if (!skipIarDownload) {
--- playwright/tests/utils/download-iar.ts
+++ playwright/tests/utils/download-iar.ts
@@ -5,7 +5,7 @@ import { emptyDownloadIarFilenameUpToDate as targetIarFilename, empty as targetI
 
 const downloadIar = async (
   urlZipContainingIars: string,
-  filenameMustContain: string[],
+  patternsMustContain: string[],
   logger: (message: string) => void
 ): Promise<void> => {
   const targetPath = path.join(targetIarPath, targetIarFilename);
@@ -20,7 +20,7 @@ const downloadIar = async (
     const zipPathFilename = path.basename(zipPath);
     if (zipEntry.dir) continue;
     if (!zipPath.toLowerCase().endsWith('.iar')) continue;
-    if (filenameMustContain.every(substr => zipPathFilename.includes(substr))) {
+    if (patternsMustContain.every(substr => zipPathFilename.includes(substr))) {
       logger(`Found matching IAR file: ${zipPath} with filename ${zipPathFilename}`);
       iarFileToCopy = {
         path: zipPath,
@@ -31,7 +31,7 @@ const downloadIar = async (
   }
 
   if (!iarFileToCopy) {
-    throw new Error(`No IAR file in ZIP found containing all patterns ${filenameMustContain}`);
+    throw new Error(`No IAR file in ZIP found containing all patterns ${patternsMustContain}`);
   }
 
   try {
@@ -48,10 +48,13 @@ const downloadIar = async (
 };
 
 export const runDownloadIar = async () => {
-  const url = process.argv[2]
-    ? process.argv[2]
+  console.log('runDownloadIar: process.argv: ' + process.argv.join('\n'));
+  console.log('runDownloadIar: process.env: ' + JSON.stringify(process.env, null, 2));
+
+  const url = process.env.IAR_DOWNLOAD_URL
+    ? process.env.IAR_DOWNLOAD_URL
     : 'https://jenkins.ivyteam.io/job/demo-projects/job/master/lastSuccessfulBuild/artifact/connectivity/connectivity-demos/target/*zip*/target.zip';
-  const rawPattern = process.argv[3] ? process.argv[3] : 'connectivity-demos,SNAPSHOT';
+  const rawPattern = process.env.IAR_FILE_MUST_CONTAIN ? process.env.IAR_FILE_MUST_CONTAIN : 'connectivity-demos,SNAPSHOT';
   try {
     const patterns = rawPattern
       .split(',')
