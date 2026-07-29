dd344767268252335dabecc8dad02274e8de4415
ivy-team <info@ivyteam.ch>
2026-07-29 11:52:22 +0000

    Raise ivy projects to latest version

--- playwright/tests/workspaces/prebuiltProject/src_generated/dataclass/prebuiltProject/RuntimeLogData.java
+++ playwright/tests/workspaces/prebuiltProject/src_generated/dataclass/prebuiltProject/RuntimeLogData.java
@@ -3,7 +3,7 @@ package prebuiltProject;
 /**
  */
 @SuppressWarnings("all")
-@javax.annotation.processing.Generated(comments="This is the java file of the ivy data class RuntimeLogData", value={"ch.ivyteam.ivy.scripting.streamInOut.IvyScriptJavaClassBuilder"})
+@jakarta.annotation.Generated(comments="This is the java file of the ivy data class RuntimeLogData", value={"ch.ivyteam.ivy.scripting.streamInOut.IvyScriptJavaClassBuilder"})
 public class RuntimeLogData extends ch.ivyteam.ivy.scripting.objects.CompositeObject
 {
   /** SerialVersionUID */
