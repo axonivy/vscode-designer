4f2dc90907a739cade43c0754d34ed42b7bdef61
Lukas Giger <141232142+ivy-lgi@users.noreply.github.com>
2026-08-03 14:57:03 +0200

    XIVY-19179 fix tests after project structure improvements

---
+++ playwright/tests/workspaces/prebuiltProject/target/dataclass/prebuiltProject/DataClassEditorTest.java
@@ -0,0 +1,12 @@
+package prebuiltProject;
+
+/**
+ */
+@SuppressWarnings("all")
+@jakarta.annotation.Generated(comments="This is the java file of the ivy data class DataClassEditorTest", value={"ch.ivyteam.ivy.scripting.streamInOut.IvyScriptJavaClassBuilder"})
+public class DataClassEditorTest extends ch.ivyteam.ivy.scripting.objects.CompositeObject
+{
+  /** SerialVersionUID */
+  private static final long serialVersionUID = 0L;
+
+}
