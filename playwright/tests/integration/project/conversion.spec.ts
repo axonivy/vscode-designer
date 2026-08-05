3f2a7c3184644e0bf69b04c887551be7b738bbfe
ivy-dar <dominik.arnold@axonivy.com>
2026-08-05 11:34:21 +0200

    add wait before checking the log

--- playwright/tests/integration/project/conversion.spec.ts
+++ playwright/tests/integration/project/conversion.spec.ts
@@ -24,6 +24,7 @@ test.only('Convert project', async ({ wsPage }) => {
   const successToast = wsPage.toasts.filter({ hasText: new RegExp('Converted 1 of 1 Axon Ivy project\\(s\\)') });
   await expect(successToast).toBeVisible();
 
+  await wsPage.page.waitForTimeout(5_000); // wait for conversion to finish
   const output = new OutputView(wsPage);
   await output.view.press('ControlOrMeta+End');
   await output.expectLogEntry('[info] Finished conversion of project playwrightTestWorkspace');
