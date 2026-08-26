8b57ca27fc2299ee66172b837c9a69def61535a4
Louis <louis.mueller@axonivy.com>
2026-08-26 08:49:47 +0200

    editors: call initialize where available (#1368)

--- webviews/casemap-editor/src/index.tsx
+++ webviews/casemap-editor/src/index.tsx
@@ -20,13 +20,15 @@ export async function start({ file }: InitializeConnection) {
     throw new Error('Root element not found');
   }
   initTranslation();
+  const context = { app: '', project: '', file };
+  await client.initialize(context)
   createRoot(rootElement).render(
     <React.StrictMode>
       <ThemeProvider disabled={true}>
         <ClientContextProvider client={client}>
           <QueryProvider client={queryClient}>
             <HotkeysProvider initiallyActiveScopes={['global']}>
-              <CaseMapEditor context={{ app: '', project: '', file }} />
+              <CaseMapEditor context={context} />
               <Toaster closeButton={true} />
             </HotkeysProvider>
           </QueryProvider>
