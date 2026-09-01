a279a04728a1e3bce92973a31bf98ddcaf47a452
ivy-dar <dominik.arnold@axonivy.com>
2026-09-01 16:53:43 +0200

    add comment

--- extension/src/engine/engine-release-train.ts
+++ extension/src/engine/engine-release-train.ts
@@ -13,7 +13,8 @@ export const engineReleaseTrain = () => {
     return train;
   }
   return extensionVersion.isPreview
-    ? extensionVersion.patch > 1_000_000_000 || extensionVersion.patch == 0
+    ? // FIXME: Ugly workaround classifies each extension version with a patch > 0 and not a YYYYMMDD as a milestone release. Could lead to problems in the future.
+      extensionVersion.patch > 1_000_000_000 || extensionVersion.patch == 0
       ? 'nightly'
       : 'milestone'
     : `${extensionVersion.major}.${extensionVersion.minor}`;
