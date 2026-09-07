07ea3280c6fcc4ed5367194d392209fe9df52fbc
Louis <louis.mueller@axonivy.com>
2026-09-07 15:42:33 +0200

    wyy

--- playwright/tests/integration/editor/xhtml.spec.ts
+++ playwright/tests/integration/editor/xhtml.spec.ts
@@ -10,8 +10,7 @@ test('xhtml completions', async ({ wsPage }) => {
   await editor.expectCompletionAtLineColumn('h:body', 19, 10);
 });
 
-// eslint-disable-next-line playwright/no-focused-test
-test.only('xhtml definitions', async ({ wsPage }) => {
+test('xhtml definitions', async ({ wsPage }) => {
   test.setTimeout(60_000); // slow test due to java activation
   const editor = new XhtmlEditor(wsPage);
   await editor.open();
