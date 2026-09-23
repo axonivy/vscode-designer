fcb73b974e1777c91bacfc314508c22c6b59274a
ivy-dar <dominik.arnold@axonivy.com>
2026-09-23 16:08:17 +0200

    XIVY-19449 Fix new dialogs via context menu not working

--- extension/src/project-explorer/ivy-project-explorer.ts
+++ extension/src/project-explorer/ivy-project-explorer.ts
@@ -78,9 +78,9 @@ export class IvyProjectExplorer {
     registerCmd(`${VIEW_ID}.installMarketProduct`, (s: TreeSelection) => this.installMarketProduct(s));
 
     registerCmd(`${VIEW_ID}.addNewProject`, (s: TreeSelection) => this.addProject(s));
-    registerCmd(`${VIEW_ID}.addNewHtmlDialog`, (s: TreeSelection, pid?: string) => this.addUserDialog(s, 'JSF', pid));
-    registerCmd(`${VIEW_ID}.addNewFormDialog`, (s: TreeSelection, pid?: string) => this.addUserDialog(s, 'Form', pid));
-    registerCmd(`${VIEW_ID}.addNewOfflineDialog`, (s: TreeSelection, pid?: string) => this.addUserDialog(s, 'JSFOffline', pid));
+    registerCmd(`${VIEW_ID}.addNewHtmlDialog`, (s: TreeSelection) => this.addUserDialog(s, 'JSF'));
+    registerCmd(`${VIEW_ID}.addNewFormDialog`, (s: TreeSelection) => this.addUserDialog(s, 'Form'));
+    registerCmd(`${VIEW_ID}.addNewOfflineDialog`, (s: TreeSelection) => this.addUserDialog(s, 'JSFOffline'));
     registerCmd(`${VIEW_ID}.addNewDataClass`, (s: TreeSelection) => this.addDataClass(s));
     registerCmd(`${VIEW_ID}.addNewEntityClass`, (s: TreeSelection) => this.addEntityClass(s));
     registerCmd(`${VIEW_ID}.addNewCaseMap`, (s: TreeSelection) => this.addCaseMap(s));
