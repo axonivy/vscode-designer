f49d9da00ceb5f06e8f95ccfcd55c6eab6a6f0ea
ivy-dar <dominik.arnold@axonivy.com>
2026-09-23 10:03:01 +0200

    unquote executable

--- extension/src/base/maven-version-validation.ts
+++ extension/src/base/maven-version-validation.ts
@@ -90,7 +90,8 @@ const getMvnExecutables = () => {
 
 const checkMvnExecutable = async (executable: string): Promise<boolean> => {
   try {
-    const command = [`"${executable}"`, '--version'].join(' ');
+    // const command = [`"${executable}"`, '--version'].join(' ');
+    const command = [executable, '--version'].join(' ');
     const { stdout, stderr } = await execAsync(command, { encoding: 'utf8', windowsHide: true });
     const version = `${stdout}${stderr}`;
     return isExpectedMavenVersion(version);
