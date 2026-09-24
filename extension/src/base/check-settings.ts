80d5042a0ccdeb6a7388259322e83e30870f08c6
ivy-dar <dominik.arnold@axonivy.com>
2026-09-24 13:39:55 +0200

    Add settings check on activation, warn if dangerous settings found

---
+++ extension/src/base/check-settings.ts
@@ -0,0 +1,20 @@
+import { workspace } from 'vscode';
+import { logWarningMessage } from './logging-util';
+
+type SettingToCheck = { key: string; expectedType: 'boolean'; expectedValue: boolean };
+
+const SETTINGS_TO_CHECK: SettingToCheck[] = [{ key: 'java.import.maven.enabled', expectedType: 'boolean', expectedValue: true }];
+
+export const checkSettings = () => {
+  SETTINGS_TO_CHECK.forEach(setting => {
+    const settingEffective = workspace.getConfiguration().get<unknown>(setting.key);
+    if (
+      settingEffective !== undefined &&
+      (typeof settingEffective !== setting.expectedType || settingEffective !== setting.expectedValue)
+    ) {
+      logWarningMessage(`Dangerous setting override found for setting "${setting.key}".
+        Expected ${JSON.stringify(setting.expectedValue)} (${setting.expectedType}) but found ${JSON.stringify(settingEffective)} (${typeof settingEffective}).
+        Remove the setting to ensure the extension works correctly.`);
+    }
+  });
+};
