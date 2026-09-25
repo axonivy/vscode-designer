f9cb6f728a296b4c094a2377c82a8c28df2fba28
ivy-dar <dominik.arnold@axonivy.com>
2026-09-25 15:15:05 +0200

    focus playwright test on market

--- playwright/tests/integration/market/install-product.spec.ts
+++ playwright/tests/integration/market/install-product.spec.ts
@@ -2,90 +2,93 @@ import { expect, test } from '~/fixtures/baseTest';
 import { FileExplorer } from '~/page-objects/explorer-view';
 import { ProcessEditor } from '~/page-objects/process-editor';
 
-test('Install product without maven-dependency from Market website', async ({ wsPage }) => {
-  const explorer = new FileExplorer(wsPage);
-  await explorer.installProduct('connectivity-demo');
-  await wsPage.provideUserInput('14.0.0');
-  const header = wsPage.page.locator('div.quick-input-header');
-  const checkbox = header.getByRole('checkbox', { name: 'Toggle all checkboxes' });
-  await checkbox.check();
-  await wsPage.provideUserInput();
-  await wsPage.executeCommand('Refresh Explorer');
-  await explorer.selectNodeExact('connectivity-demos');
-  const processEditor = new ProcessEditor(wsPage, 'personService.p.json');
-  await processEditor.open();
-});
+// eslint-disable-next-line playwright/no-focused-test
+test.describe.only('Market Product Installation', () => {
+  test('Install product without maven-dependency from Market website', async ({ wsPage }) => {
+    const explorer = new FileExplorer(wsPage);
+    await explorer.installProduct('connectivity-demo');
+    await wsPage.provideUserInput('14.0.0');
+    const header = wsPage.page.locator('div.quick-input-header');
+    const checkbox = header.getByRole('checkbox', { name: 'Toggle all checkboxes' });
+    await checkbox.check();
+    await wsPage.provideUserInput();
+    await wsPage.executeCommand('Refresh Explorer');
+    await explorer.selectNodeExact('connectivity-demos');
+    const processEditor = new ProcessEditor(wsPage, 'personService.p.json');
+    await processEditor.open();
+  });
 
-test('Install product with maven-dependency from Market website', async ({ wsPage }) => {
-  const explorer = new FileExplorer(wsPage);
-  await explorer.installProduct('excel-connector');
-  await wsPage.provideUserInput('13.1.2');
-  const header = wsPage.page.locator('div.quick-input-header');
-  const checkbox = header.getByRole('checkbox', { name: 'Toggle all checkboxes' });
-  await checkbox.check();
-  await wsPage.provideUserInput();
-  await expect(wsPage.quickInputBox).toBeVisible();
-  await wsPage.provideUserInput('playwrightTestWorkspace');
-  await wsPage.executeCommand('Refresh Explorer');
-  await explorer.selectNodeExact('excel-connector-demo');
-});
+  test('Install product with maven-dependency from Market website', async ({ wsPage }) => {
+    const explorer = new FileExplorer(wsPage);
+    await explorer.installProduct('excel-connector');
+    await wsPage.provideUserInput('13.1.2');
+    const header = wsPage.page.locator('div.quick-input-header');
+    const checkbox = header.getByRole('checkbox', { name: 'Toggle all checkboxes' });
+    await checkbox.check();
+    await wsPage.provideUserInput();
+    await expect(wsPage.quickInputBox).toBeVisible();
+    await wsPage.provideUserInput('playwrightTestWorkspace');
+    await wsPage.executeCommand('Refresh Explorer');
+    await explorer.selectNodeExact('excel-connector-demo');
+  });
 
-test('Install product with conflicting project folder', async ({ wsPage }) => {
-  const explorer = new FileExplorer(wsPage);
-  await explorer.installProduct('connectivity-demo');
-  await wsPage.provideUserInput('14.0.0');
-  const header = wsPage.page.locator('div.quick-input-header');
-  const checkbox = header.getByRole('checkbox', { name: 'Toggle all checkboxes' });
-  await checkbox.check();
-  await wsPage.provideUserInput();
-  await wsPage.hasReadyStatusMessage();
-  await explorer.installProduct('connectivity-demo');
-  await wsPage.provideUserInput('14.0.0');
-  const title = wsPage.page.locator('div.quick-input-title');
-  await expect(title).toHaveText(/The following projects cannot be installed because a project folder with the same name already exists/);
-});
+  test('Install product with conflicting project folder', async ({ wsPage }) => {
+    const explorer = new FileExplorer(wsPage);
+    await explorer.installProduct('connectivity-demo');
+    await wsPage.provideUserInput('14.0.0');
+    const header = wsPage.page.locator('div.quick-input-header');
+    const checkbox = header.getByRole('checkbox', { name: 'Toggle all checkboxes' });
+    await checkbox.check();
+    await wsPage.provideUserInput();
+    await wsPage.hasReadyStatusMessage();
+    await explorer.installProduct('connectivity-demo');
+    await wsPage.provideUserInput('14.0.0');
+    const title = wsPage.page.locator('div.quick-input-title');
+    await expect(title).toHaveText(/The following projects cannot be installed because a project folder with the same name already exists/);
+  });
 
-test('Install local product.json', async ({ wsPage }) => {
-  const explorer = new FileExplorer(wsPage);
-  await explorer.selectNode('resources');
-  await explorer.selectNode('product.json');
-  await explorer.installLocalProduct('product.json');
-  await wsPage.provideUserInput();
-  await wsPage.executeCommand('Refresh Explorer');
-  await explorer.selectNode('connectivity-demos');
-  const processEditor = new ProcessEditor(wsPage, 'personService.p.json');
-  await processEditor.open();
-});
+  test('Install local product.json', async ({ wsPage }) => {
+    const explorer = new FileExplorer(wsPage);
+    await explorer.selectNode('resources');
+    await explorer.selectNode('product.json');
+    await explorer.installLocalProduct('product.json');
+    await wsPage.provideUserInput();
+    await wsPage.executeCommand('Refresh Explorer');
+    await explorer.selectNode('connectivity-demos');
+    const processEditor = new ProcessEditor(wsPage, 'personService.p.json');
+    await processEditor.open();
+  });
 
-test('Install local product.json with dynamic version', async ({ wsPage }) => {
-  const explorer = new FileExplorer(wsPage);
-  await explorer.selectNode('resources');
-  await explorer.selectNode('product-dynamic.json');
-  await explorer.installLocalProduct('product-dynamic.json');
-  await wsPage.provideUserInput('14.0.0-SNAPSHOT');
+  test('Install local product.json with dynamic version', async ({ wsPage }) => {
+    const explorer = new FileExplorer(wsPage);
+    await explorer.selectNode('resources');
+    await explorer.selectNode('product-dynamic.json');
+    await explorer.installLocalProduct('product-dynamic.json');
+    await wsPage.provideUserInput('14.0.0-SNAPSHOT');
 
-  const projects = wsPage.quickInputList;
-  await expect(projects).toBeVisible();
-  const entry = projects.locator('div.quick-input-list-entry');
-  await expect(entry).toHaveCount(1);
-  await expect(entry.getByRole('checkbox')).toHaveAttribute('aria-label', '👁️ connectivity-demos (com.axonivy.demo)');
+    const projects = wsPage.quickInputList;
+    await expect(projects).toBeVisible();
+    const entry = projects.locator('div.quick-input-list-entry');
+    await expect(entry).toHaveCount(1);
+    await expect(entry.getByRole('checkbox')).toHaveAttribute('aria-label', '👁️ connectivity-demos (com.axonivy.demo)');
 
-  await wsPage.provideUserInput();
-  await wsPage.executeCommand('Refresh Explorer');
-  await explorer.selectNode('connectivity-demos');
-  const processEditor = new ProcessEditor(wsPage, 'personService.p.json');
-  await processEditor.open();
-});
+    await wsPage.provideUserInput();
+    await wsPage.executeCommand('Refresh Explorer');
+    await explorer.selectNode('connectivity-demos');
+    const processEditor = new ProcessEditor(wsPage, 'personService.p.json');
+    await processEditor.open();
+  });
 
-test('Install local product.json with conflicting project folder', async ({ wsPage }) => {
-  const explorer = new FileExplorer(wsPage);
-  await explorer.selectNode('resources');
-  await explorer.selectNode('product.json');
-  await explorer.installLocalProduct('product.json');
-  await wsPage.provideUserInput();
-  await wsPage.hasReadyStatusMessage();
-  await explorer.installLocalProduct('product.json');
-  await wsPage.provideUserInput();
-  const title = wsPage.page.locator('div.quick-input-title');
-  await expect(title).toHaveText(/The following projects cannot be installed because a project folder with the same name already exists/);
+  test('Install local product.json with conflicting project folder', async ({ wsPage }) => {
+    const explorer = new FileExplorer(wsPage);
+    await explorer.selectNode('resources');
+    await explorer.selectNode('product.json');
+    await explorer.installLocalProduct('product.json');
+    await wsPage.provideUserInput();
+    await wsPage.hasReadyStatusMessage();
+    await explorer.installLocalProduct('product.json');
+    await wsPage.provideUserInput();
+    const title = wsPage.page.locator('div.quick-input-title');
+    await expect(title).toHaveText(/The following projects cannot be installed because a project folder with the same name already exists/);
+  });
 });
